07e34c81f1e681fa05b56d52fab12c1bbcaf6369
Nana250-coda <elnorajimeto1234@gmail.com>
2026-07-10 01:55:18 +0100

    Update database configuration

--- config/db.config.js
+++ config/db.config.js
@@ -8,4 +8,11 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const CONNECTIONLIMIT = process.env.DB_CONNECTIONLIMIT;
 
+console.log("DB Config:");
+console.log({
+  HOST,
+  PORT,
+  DATABASE,
+  USERNAME,
+});
 module.exports = {DATABASE, HOST, USERNAME, PASSWORD, PORT, CONNECTIONLIMIT}
